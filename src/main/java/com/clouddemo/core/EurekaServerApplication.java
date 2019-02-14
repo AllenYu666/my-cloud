a7e884a3c0ed0171f83c367eb58b62d72605e3d3
AllenYu666 <45704734+AllenYu666@users.noreply.github.com>
2019-02-14 22:31:03 +0800

    Update EurekaServerApplication.java

--- src/main/java/com/clouddemo/core/EurekaServerApplication.java
+++ src/main/java/com/clouddemo/core/EurekaServerApplication.java
@@ -10,7 +10,7 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		/**
-		*6666666666666
+		*66666666666677776
 		*/
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
